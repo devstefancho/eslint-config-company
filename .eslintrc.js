fee11c93c045fcfba866b1d0fd7c96458db0d6ab
stefancho <stefancho@joseongjins-MacBook-Pro.local>
2022-05-22 17:01:09 +0900

    remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     "next/core-web-vitals",
-    "plugin:prettier/recommended" // eslint-plugin-prettier 을 여기에서 미리 설치해줌 (prettier config가 다른 formatting rule을 override 하게 해주는 것)
   ],
   env: {
     browser: true,
@@ -16,17 +15,6 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-  },
-  rules: {
-    "prettier/prettier": [
-      'error',
-      {
-        "printWidth": 80,
-        "singleQuote": true,
-        "trailingComma": "es5",
-        "tabWidth": 8 // test를 위해서 8로 해둠
-      }
-    ]
   },
   settings: {
     react: {
